aee22e2ea72078c6fa515ba0dfd8ed736c16750d
Adam Brown <adamwbrown@gmail.com>
2024-02-04 21:11:36 -0800

    Publish signing

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 allprojects {
-    group = "org.jetbrains.kotlinx.multiplatform-library-template"
-    version = "0.0.1"
+    group = "com.darkrockstudios.symspellkt"
+    version = "0.1.0"
 }
 
 nexusPublishing {
